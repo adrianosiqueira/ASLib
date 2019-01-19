52a83e8a0e0e03878fadde153fa1715059f1f441
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-01-18 22:27:57 -0200

    Update metadata.

--- src/aslib/exceptions/InvalidClassException.java
+++ src/aslib/exceptions/InvalidClassException.java
@@ -4,6 +4,8 @@ package aslib.exceptions;
  * <p> Custom exception to symbolize that some class is invalid to the context. </p>
  *
  * @author Adriano Siqueira
+ * @version 1.0
+ * @since 4.2
  */
 public class InvalidClassException extends RuntimeException {
     public InvalidClassException() {
