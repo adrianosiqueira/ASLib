e5a399f14e4eba86e2794fa82fbacc7645729745
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-01-21 21:42:10 -0200

    New exception.

---
+++ src/aslib/exceptions/InvalidDateException.java
@@ -0,0 +1,29 @@
+package aslib.exceptions;
+
+/**
+ * <p> Custom exception to symbolize that some date is invalid. </p>
+ *
+ * @author Adriano Siqueira
+ * @version 1.0
+ * @since 5.2
+ */
+public class InvalidDateException extends RuntimeException {
+    public InvalidDateException() {
+    }
+
+    public InvalidDateException(String message) {
+        super(message);
+    }
+
+    public InvalidDateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidDateException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidDateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
