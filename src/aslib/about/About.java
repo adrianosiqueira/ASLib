0087935c25a9d53641fae12781c23f818ef7ec73
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-01-26 17:08:26 -0200

    Update data.

--- src/aslib/about/About.java
+++ src/aslib/about/About.java
@@ -4,17 +4,19 @@ package aslib.about;
  * <p> Show info about the aslib. </p>
  *
  * @author Adriano Siqueira
+ * @version 1.1
+ * @since 4.2
  */
 public class About {
-    public static final String VERSION = "5.1";
-    public static final String DATE = "18/jan/2019";
+    public static final String VERSION = "6.0";
+    public static final String DATE = "26/jan/2019";
 
     /**
      * <p> Show info about the aslib. </p>
      */
     public static void show() {
-        System.out.println("Library");
+        System.out.println("       AS Lib");
         System.out.println("Version: " + VERSION);
-        System.out.println("Date: " + DATE);
+        System.out.println("   Date: " + DATE);
     }
 }
