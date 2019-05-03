c21b2ccbe2a1d13313ef735da478935a8a34fdbf
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-05-03 16:43:25 -0300

    Ported from java 10 to 8.

--- src/aslib/util/GenderType.java
+++ src/aslib/util/GenderType.java
@@ -3,10 +3,46 @@ package aslib.util;
 import aslib.exceptions.InvalidEnumSearchArgumentException;
 
 /**
- * <p> Contains the options to represent the gender of a person. </p>
+ * <p> Contains the options to represent the code of a person. </p>
+ *
+ * <h2> Codes </h2>
+ * <table style="width:120px" summary="">
+ * <tr>
+ * <th> Type </th>
+ * <th> Code </th>
+ * </tr>
+ * <tr>
+ * <td> Invalid </td>
+ * <td> -1 </td>
+ * </tr>
+ * <tr>
+ * <td> Undefined </td>
+ * <td> 0 </td>
+ * </tr>
+ * <tr>
+ * <td> Feminine </td>
+ * <td> 1 </td>
+ * </tr>
+ * <tr>
+ * <td> Masculine </td>
+ * <td> 2 </td>
+ * </tr>
+ * <tr>
+ * <td> Lesbian </td>
+ * <td> 3 </td>
+ * </tr>
+ * <tr>
+ * <td> Gay </td>
+ * <td> 4 </td>
+ * </tr>
+ * <tr>
+ * <td> Transgender </td>
+ * <td> 5 </td>
+ * </tr>
+ * </table>
  *
  * @author Adriano Siqueira
- * @version 1.0
+ * @version 2019-05-03
  * @since 6.0
  */
 public enum GenderType {
@@ -18,33 +54,24 @@ public enum GenderType {
     GAY(4),
     TRANSGENDER(5);
 
-    private int gender;
+    public final int code;
 
-    GenderType(int gender) {
-        this.gender = gender;
+    GenderType(int code) {
+        this.code = code;
     }
 
     /**
      * <p> Search for the option whose code matches the argument. </p>
      *
-     * @param gender Code to return as an option of enum.
+     * @param code Code to return as an option of enum.
      * @return The enum option that matches the code.
      * @throws InvalidEnumSearchArgumentException If the code not exists.
      */
-    public GenderType getGenderType(int gender) throws InvalidEnumSearchArgumentException {
+    public static GenderType getByCode(int code) throws InvalidEnumSearchArgumentException {
         for (GenderType value : values())
-            if (value.gender == gender)
+            if (value.code == code)
                 return value;
 
-        throw new InvalidEnumSearchArgumentException("The gender provided is invalid!");
-    }
-
-    /**
-     * <p> Returns the code of the enum option. </p>
-     *
-     * @return The code of gender.
-     */
-    public int getGender() {
-        return gender;
+        throw new InvalidEnumSearchArgumentException("The code provided is invalid!");
     }
 }
