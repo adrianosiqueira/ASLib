3ff56e8ec4d9b7271de78def6503c76329ec51e2
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-05-03 16:18:01 -0300

    New extensions and function to get the object.

--- src/aslib/filemanager/FileExtension.java
+++ src/aslib/filemanager/FileExtension.java
@@ -1,5 +1,6 @@
 package aslib.filemanager;
 
+import aslib.exceptions.InvalidEnumSearchArgumentException;
 import javafx.stage.FileChooser;
 
 /**
@@ -8,8 +9,60 @@ import javafx.stage.FileChooser;
  * <p> Automatically creates the {@link javafx.stage.FileChooser.ExtensionFilter}
  * from the selected option. </p>
  *
+ * <h2> Filters and Extensions </h2>
+ * <table style="width:400px" summary="">
+ * <tr>
+ * <th> Filter </th>
+ * <th> Extensions </th>
+ * </tr>
+ * <tr>
+ * <td> All </td>
+ * <td> * </td>
+ * </tr>
+ * <tr>
+ * <td> Compressed </td>
+ * <td> *.7z, *.ace, *.alz, *.arc, *.arj, *.bzip2, *.egg, *.gz, *.gzip, *.rar, *.tar, *.zip </td>
+ * </tr>
+ * <tr>
+ * <td> Disk Image </td>
+ * <td> *.adf, *.bin, *.cue, *.dmg, *.img, *.iso, *.nrg </td>
+ * </tr>
+ * <tr>
+ * <td> Document </td>
+ * <td> *.doc, *.docx, *.ppt, *.pptx, *.xls, *.xlsx, *.odt, *.odp, *.ods, *.pdf, *.rtf, *.txt </td>
+ * </tr>
+ * <tr>
+ * <td> Hash </td>
+ * <td> *.txt, *.md5, *.sha1, *.sha224, *.sha356, *.sha384, *.sha512 </td>
+ * </tr>
+ * <tr>
+ * <td> Music </td>
+ * <td> *.aac, *.aiff, *.mp3, *.ogg, *.wav, *.wma </td>
+ * </tr>
+ * <tr>
+ * <td> Picture </td>
+ * <td> *.gif, *.jpg, *.jpeg, *.png, *.tiff </td>
+ * </tr>
+ * <tr>
+ * <td> Runnable </td>
+ * <td> *.apk, *.appimage, *.AppImage, *.exe, *.jar, *.run </td>
+ * </tr>
+ * <tr>
+ * <td> Script </td>
+ * <td> *.bash, *.bat, *.dat, *.sh, *.vbs </td>
+ * </tr>
+ * <tr>
+ * <td> Source Code </td>
+ * <td> *.c, *.cpp, *.java, *.pas </td>
+ * </tr>
+ * <tr>
+ * <td> Video </td>
+ * <td> *.flv, *.mkv, *.mp4, *.mpeg, *.wmv </td>
+ * </tr>
+ * </table>
+ *
  * @author Adriano Siqueira
- * @version 1.0
+ * @version 1.0 - 1.1
  * @since 6.0
  */
 public enum FileExtension {
@@ -41,7 +94,7 @@ public enum FileExtension {
     /**
      * <p> Only music files are shown. </p>
      */
-    MUSIC("Musics", "*.aac", "*.aiff", "*.mp3", "*.ogg", "*.wav", "*.wma"),
+    MUSIC("Musics", "*.aac", "*.aiff", "*.mp3", "*.oga", "*.ogg", "*.wav", "*.wma"),
 
     /**
      * <p> Only picture files are shown. </p>
@@ -61,12 +114,12 @@ public enum FileExtension {
     /**
      * <p> Only source code files are shown. </p>
      */
-    SOURCE_CODE("Source Codes", "*.c", "*.cpp", "*.java", "*.pas"),
+    SOURCE_CODE("Source Codes", "*.c", "*.cpp", "*.java", "*.pas", "*.py"),
 
     /**
      * <p> Only video files are shown. </p>
      */
-    VIDEO("Videos", "*.flv", "*.mkv", "*.mp4", "*.mpeg", "*.wmv");
+    VIDEO("Videos", "*.flv", "*.mkv", "*.mp4", "*.mpeg", "*.ogv", "*.rmvb", "*.wmv");
 
     public final String description;
     public final String[] extensions;
@@ -76,5 +129,29 @@ public enum FileExtension {
         this.description = description;
         this.extensions = extensions;
         this.filter = new FileChooser.ExtensionFilter(description, extensions);
+    }
+
+    /**
+     * <p> Searches for the extension at all enum options and returns the first
+     * occurrence. </p>
+     *
+     * @param extension Extension to be searched for.
+     * @return The {@link FileExtension} option that contains the extension in its list.
+     * @throws IllegalArgumentException           If the extension does not starts with a dot.
+     * @throws InvalidEnumSearchArgumentException If the extension does not exists.
+     * @throws NullPointerException               If the extension is null.
+     */
+    public static FileExtension getByExtension(String extension) throws IllegalArgumentException, InvalidEnumSearchArgumentException, NullPointerException {
+        if (extension == null)
+            throw new NullPointerException("The extension can not be null");
+        else if (extension.charAt(0) != '.')
+            throw new IllegalArgumentException("The extension must start with a dot ('.').");
+
+        for (FileExtension value : values())
+            for (String valueExtension : value.extensions)
+                if (valueExtension.endsWith(extension))
+                    return value;
+
+        throw new InvalidEnumSearchArgumentException("The extension does not exists.");
     }
 }
