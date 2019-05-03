13394188747a229970fa727dd9272ae3da7d2e6f
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-05-03 17:51:58 -0300

    Only one function to handle conversion.

--- src/aslib/util/MorseStringDictionary.java
+++ src/aslib/util/MorseStringDictionary.java
@@ -1,93 +1,45 @@
 package aslib.util;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * <p> Contains the functions to convert Morse code into String. </p>
  *
  * @author Adriano Siqueira
- * @version 1.0
+ * @version 2019-05-03
  * @since 6.0
  */
-public class MorseStringDictionary {
-    private static Map<String, Character> dictionary;
+public class MorseStringDictionary implements MorseDictionary {
 
-    static {
-        dictionary = new HashMap<>();
-        dictionary.put("- - - - -", '0');
-        dictionary.put(". - - - -", '1');
-        dictionary.put(". . - - -", '2');
-        dictionary.put(". . . - -", '3');
-        dictionary.put(". . . . -", '4');
-        dictionary.put(". . . . .", '5');
-        dictionary.put("- . . . .", '6');
-        dictionary.put("- - . . .", '7');
-        dictionary.put("- - - . .", '8');
-        dictionary.put("- - - - .", '9');
-        dictionary.put(". -", 'A');
-        dictionary.put("- . . .", 'B');
-        dictionary.put("- . - .", 'C');
-        dictionary.put("- . .", 'D');
-        dictionary.put(".", 'E');
-        dictionary.put(". . - .", 'F');
-        dictionary.put("- - .", 'G');
-        dictionary.put(". . . .", 'H');
-        dictionary.put(". .", 'I');
-        dictionary.put(". - - -", 'J');
-        dictionary.put("- . -", 'K');
-        dictionary.put(". - . .", 'L');
-        dictionary.put("- -", 'M');
-        dictionary.put("- .", 'N');
-        dictionary.put("- - -", 'O');
-        dictionary.put(". - - .", 'P');
-        dictionary.put("- - . -", 'Q');
-        dictionary.put(". - .", 'R');
-        dictionary.put(". . .", 'S');
-        dictionary.put("-", 'T');
-        dictionary.put(". . -", 'U');
-        dictionary.put(". . . -", 'V');
-        dictionary.put(". - -", 'W');
-        dictionary.put("- . . -", 'X');
-        dictionary.put("- . - -", 'Y');
-        dictionary.put("- - . .", 'Z');
-        dictionary.put("       ", ' ');
-    }
-
-    /**
-     * <p> Search for the code and returns the letter for it. </p>
-     *
-     * @param code Code to be converted.
-     * @return The letter referring to the code or null if the code is not found.
-     */
-    public char getStringLetter(String code) {
-        return dictionary.get(code);
-    }
-
-    /**
-     * <p> Converts the entire message. </p>
-     *
-     * @param message Message to be converted.
-     * @return Entire message converted to morse code or an empty string if the message is null.
-     */
-    public String getStringMessage(String message) {
-        if (message == null)
+    @Override
+    public String get(String morse) {
+        if (morse == null)
             return "";
 
-        StringBuilder converted = new StringBuilder();
-        String[] words = message.split(" {7}");
+        StringBuilder message = new StringBuilder();
+        String[] words = morse.split(" {7}");
 
         for (String word : words) {
             String[] letters = word.split(" {3}");
 
-            for (String letter : letters)
-                converted.append(dictionary.get(letter));
+            for (String letter : letters) {
+                String s = "";
 
-            converted.append(" ");
+                for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                    if (entry.getValue().equals(letter)) {
+                        s = entry.getKey();
+                        break;
+                    }
+                }
+
+                message.append(s);
+            }
+
+            message.append(" ");
         }
 
         // Remove redundant white space
-        converted.deleteCharAt(converted.length() - 1);
-        return converted.toString();
+        message.deleteCharAt(message.length() - 1);
+        return message.toString();
     }
 }
