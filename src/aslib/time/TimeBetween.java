8522b7a3af76ca25c02d9d08f8625d0e17faf584
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-07-12 13:05:46 -0300

    Code cleanup.

--- src/aslib/time/TimeBetween.java
+++ src/aslib/time/TimeBetween.java
@@ -2,51 +2,23 @@ package aslib.time;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * <p> Contains the functions to calculate the difference between two dates. </p>
  *
  * @author Adriano Siqueira
- * @version 2019-05-03
+ * @version 2019-07-12
  * @since 6.0
  */
 public class TimeBetween {
     private LocalDate startDate;
     private LocalDate endDate;
 
-    /**
-     * <p> Creates an instance of {@link TimeBetween} class. </p>
-     *
-     * @param startDate Start date for processing. If it comes after the end date, the result will be negative.
-     * @param endDate   End date for processing.
-     */
-    public TimeBetween(Date startDate, Date endDate) {
-        if (startDate != null)
-            this.startDate = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-        if (endDate != null)
-            this.endDate = endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-    }
-
-    /**
-     * <p> Creates an instance of {@link TimeBetween} class. </p>
-     *
-     * @param startDate Start date for processing. If it comes after the end date, the result will be negative.
-     * @param endDate   End date for processing.
-     */
-    public TimeBetween(Calendar startDate, Calendar endDate) {
-        if (startDate != null)
-            this.startDate = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-        if (endDate != null)
-            this.endDate = endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-    }
+    private Period period;
 
     /**
      * <p> Creates an instance of {@link TimeBetween} class. </p>
@@ -55,8 +27,12 @@ public class TimeBetween {
      * @param endDate   End date for processing.
      */
     public TimeBetween(LocalDate startDate, LocalDate endDate) {
+        Objects.requireNonNull(startDate, "Start date can not be null.");
+        Objects.requireNonNull(startDate, "End date ca not be null.");
+
         this.startDate = startDate;
         this.endDate = endDate;
+        this.period = calculatePeriod();
     }
 
     /**
@@ -65,18 +41,14 @@ public class TimeBetween {
      *
      * <p> Each field corresponds to an integer that complements. </p>
      *
-     * <p> The DIA field complements the MONTH field, which complements the
+     * <p> The DAY field complements the MONTH field, which complements the
      * YEAR field. </p>
      *
      * @return A map with the fields as key and the values.
+     *
      * @throws NullPointerException If some of the dates are null.
      */
     public Map<ChronoUnit, Integer> getCompleteTime() throws NullPointerException {
-        if (startDate == null || endDate == null)
-            throw new NullPointerException("None of dates can be null.");
-
-        Period period = Period.between(startDate, endDate);
-
         Map<ChronoUnit, Integer> time = new HashMap<>();
         time.put(ChronoUnit.DAYS, period.getDays());
         time.put(ChronoUnit.MONTHS, period.getMonths());
@@ -89,25 +61,31 @@ public class TimeBetween {
      * <p> Calculates the amount of years between the dates. </p>
      *
      * @return The amount of years between the dates.
+     *
      * @throws NullPointerException If some of the dates are null.
      */
     public int getTotalYears() throws NullPointerException {
-        if (startDate == null || endDate == null)
-            throw new NullPointerException("None of dates can be null.");
-
-        return Period.between(startDate, endDate).getYears();
+        return period.getYears();
     }
 
     /**
      * <p> Calculates the amount of months between the dates. </p>
      *
      * @return The amount of months between the dates.
+     *
      * @throws NullPointerException If some of the dates are null.
      */
     public long getTotalMonths() throws NullPointerException {
-        if (startDate == null || endDate == null)
-            throw new NullPointerException("None of dates can be null.");
+        return period.toTotalMonths();
+    }
 
-        return Period.between(startDate, endDate).toTotalMonths();
+    /**
+     * <p> Calculates the amount time between the dates and keeps it ready to
+     * read the data as needed.. </p>
+     *
+     * @return Period calculated with the dates provided.
+     */
+    private Period calculatePeriod() {
+        return Period.between(startDate, endDate);
     }
 }
