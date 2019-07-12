1b78913a71bffc9900f2cd5878e7df96ede5483b
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-07-12 15:20:10 -0300

    New version metadata.

--- src/aslib/about/About.java
+++ src/aslib/about/About.java
@@ -8,7 +8,7 @@ package aslib.about;
  * @since 4.2
  */
 public class About {
-    public static final String VERSION = "6.1";
+    public static final String VERSION = "7.0";
     public static final String DATE = "03/may/2019";
 
     /**
