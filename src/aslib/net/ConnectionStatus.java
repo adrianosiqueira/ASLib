6246564157e10cc994540416658eff3c3adf5aec
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-07-12 12:45:02 -0300

    Now it auto checks connection status.

--- src/aslib/net/ConnectionStatus.java
+++ src/aslib/net/ConnectionStatus.java
@@ -2,11 +2,35 @@ package aslib.net;
 
 import aslib.exceptions.InvalidEnumSearchArgumentException;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * <p> Contains the options to represent the statuses of a connection. </p>
  *
+ * <h2> Codes </h2>
+ * <table>
+ *     <tr>
+ *         <th> Status </th>
+ *         <th> Code </th>
+ *     </tr>
+ *     <tr>
+ *         <td> Invalid </td>
+ *         <td> -1 </td>
+ *     </tr>
+ *     <tr>
+ *         <td> Online </td>
+ *         <td> 0 </td>
+ *     </tr>
+ *     <tr>
+ *         <td> Offline </td>
+ *         <td> 1 </td>
+ *     </tr>
+ * </table>
+ *
  * @author Adriano Siqueira
- * @version 2019-05-03
+ * @version 2019-07-12
  * @since 6.1
  */
 public enum ConnectionStatus {
@@ -24,7 +48,9 @@ public enum ConnectionStatus {
      * <p> Searches the option of enum that its status code matches the parameter. </p>
      *
      * @param status Status code to searched for.
+     *
      * @return The option of enum that status matches the argument.
+     *
      * @throws InvalidEnumSearchArgumentException If the status code does not exists.
      */
     public static ConnectionStatus getByStatus(int status) throws InvalidEnumSearchArgumentException {
@@ -33,5 +59,26 @@ public enum ConnectionStatus {
                 return value;
 
         throw new InvalidEnumSearchArgumentException("The status code does not exists.");
+    }
+
+    /**
+     * <p> Automatically checks the internet connection. </p>
+     *
+     * @return The status of the internet connection.
+     */
+    public static ConnectionStatus checkConnection() {
+        ConnectionStatus status;
+
+        try {
+            URL url = new URL("https://www.google.com");
+            URLConnection connection = url.openConnection();
+            connection.setConnectTimeout(250);
+            connection.connect();
+            status = ConnectionStatus.ONLINE;
+        } catch (IOException e) {
+            status = OFFLINE;
+        }
+
+        return status;
     }
 }
