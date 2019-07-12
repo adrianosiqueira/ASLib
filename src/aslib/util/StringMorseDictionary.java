785523077a47868c9c5bf1077bfcf63895ef017b
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2019-07-12 10:48:56 -0300

    Code cleanup.

--- src/aslib/util/StringMorseDictionary.java
+++ src/aslib/util/StringMorseDictionary.java
@@ -1,12 +1,12 @@
 package aslib.util;
 
-import java.util.Map;
+import java.util.Objects;
 
 /**
  * <p> Contains the functions to convert String into Morse code. </p>
  *
  * @author Adriano Siqueira
- * @version 2019-05-03
+ * @version 2019-07-12
  * @since 6.0
  */
 public class StringMorseDictionary implements MorseDictionary {
@@ -15,36 +15,31 @@ public class StringMorseDictionary implements MorseDictionary {
      * <p> Encodes the input message into morse code. </p>
      *
      * @param input String message to be encoded.
+     *
      * @return Entire message converted to morse code.
+     *
      * @throws NullPointerException If the input is null.
      */
+    @Override
     public String get(String input) throws NullPointerException {
-        if (input == null)
-            throw new NullPointerException("Input can not be null.");
+        Objects.requireNonNull(input, "Input can not be null.");
 
         input = input.toUpperCase();
         StringBuilder message = new StringBuilder();
 
         for (char letter : input.toCharArray()) {
-            String s = "";
-
-            for (Map.Entry<String, String> entry : dictionary.entrySet()) {
-                if (entry.getKey().equals(String.valueOf(letter))) {
-                    s = entry.getValue();
-                    break;
-                }
-            }
-
-            message.append(s);
+            String morse = dictionary.get(String.valueOf(letter));
+            message.append(morse != null ? morse : "");
 
             /* Automatically adds three spaces between letters. But when the
              * character is a white space, these three spaces will be remaining.
              * So we delete the last three white spaces in that case.
              */
-            if (letter != ' ')
+            if (letter != ' ') {
                 message.append("   ");
-            else
+            } else {
                 message.delete(message.length() - 3, message.length());
+            }
         }
 
         // Remove redundant white space
