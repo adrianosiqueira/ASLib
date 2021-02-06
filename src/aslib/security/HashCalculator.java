e1fc2169330f030c638c535d7ba3c2c5043e20f8
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-02-06 16:17:53 -0300

    Fix memory overflow.

--- src/aslib/security/HashCalculator.java
+++ src/aslib/security/HashCalculator.java
@@ -2,9 +2,10 @@ package aslib.security;
 
 import aslib.util.DataConverter;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -14,13 +15,13 @@ import java.util.Objects;
  * <p> Provides several ways to calculate the hash checksum. </p>
  *
  * @author Adriano Siqueira
- * @version 2.0.0
+ * @version 2.0.1
  * @since 9.0.0
  */
 public class HashCalculator {
 
     /**
-     * <p> Creates an {@link HashCalculator} instance. </p>
+     * <p> Creates a {@link HashCalculator} instance. </p>
      *
      * @since 2.0.0
      */
@@ -28,82 +29,96 @@ public class HashCalculator {
     }
 
     /**
-     * <p> Calculates the hash checksum from a {@link File}. </p>
+     * <p> Calculates the hash checksum from an array of bytes. </p>
      *
      * @param shaType Defines which hash algorithm will be used.
-     * @param file    What will be used to calculate the hash.
+     * @param bytes   Array which will be used to calculate the hash.
      *
      * @return The calculated hash in a String format.
      *
      * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, File file) throws NullPointerException {
+    public final String calculate(SHAType shaType, byte[] bytes)
+    throws NullPointerException {
         Objects.requireNonNull(shaType, "ShaType can not be null.");
 
-        return calculate(shaType, file.toPath());
+        try {
+            MessageDigest digest = MessageDigest.getInstance(shaType.getName());
+            byte[]        result = digest.digest(bytes);
+            return DataConverter.byteToString(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * <p> Calculates the hash checksum from a {@link File}. </p>
+     *
+     * @param shaType Defines which hash algorithm will be used.
+     * @param file    File which will be used to calculate the hash.
+     *
+     * @return The calculated hash in a String format.
+     *
+     * @throws NullPointerException If shaType is null.
+     * @since 2.0.0
+     */
+    public final String calculate(SHAType shaType, File file)
+    throws NullPointerException {
+        Objects.requireNonNull(shaType, "ShaType can not be null.");
+
+        int    bufferSize  = 2048;
+        byte[] bytesBuffer = new byte[bufferSize];
+
+        try (
+                BufferedInputStream stream = new BufferedInputStream(new FileInputStream(file),
+                                                                     bufferSize)
+        ) {
+            MessageDigest digest = MessageDigest.getInstance(shaType.getName());
+            int           read;
+
+            while ((read = stream.read(bytesBuffer)) > -1) {
+                digest.update(bytesBuffer, 0, read);
+            }
+
+            byte[] result = digest.digest();
+            return DataConverter.byteToString(result);
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
     /**
      * <p> Calculates the hash checksum from a {@link Path}. </p>
      *
      * @param shaType Defines which hash algorithm will be used.
-     * @param path    What will be used to calculate the hash.
+     * @param path    Path which will be used to calculate the hash.
      *
      * @return The calculated hash in a String format.
      *
      * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, Path path) throws NullPointerException {
-        Objects.requireNonNull(shaType, "ShaType can not be null.");
-
-        try {
-            return calculate(shaType, Files.readAllBytes(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public final String calculate(SHAType shaType, Path path)
+    throws NullPointerException {
+        return calculate(shaType, path.toFile());
     }
 
     /**
      * <p> Calculates the hash checksum from a {@link String}. </p>
      *
      * @param shaType Defines which hash algorithm will be used.
-     * @param string  What will be used to calculate the hash.
+     * @param string  String which will be used to calculate the hash.
      *
      * @return The calculated hash in a String format.
      *
      * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, String string) throws NullPointerException {
-        Objects.requireNonNull(shaType, "ShaType can not be null.");
-
+    public final String calculate(SHAType shaType, String string)
+    throws NullPointerException {
         return calculate(shaType, string.getBytes());
-    }
-
-    /**
-     * <p> Calculates the hash checksum from an array of bytes. </p>
-     *
-     * @param shaType Defines which hash algorithm will be used.
-     * @param bytes   What will be used to calculate the hash.
-     *
-     * @return The calculated hash in a String format.
-     *
-     * @throws NullPointerException If shaType is null.
-     * @since 2.0.0
-     */
-    public final String calculate(SHAType shaType, byte[] bytes) throws NullPointerException {
-        Objects.requireNonNull(shaType, "ShaType can not be null.");
-
-        try {
-            MessageDigest digest = MessageDigest.getInstance(shaType.name);
-            byte[] result = digest.digest(bytes);
-            return DataConverter.byteToString(result);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 }
