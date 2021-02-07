7f156700a860bd8ee18af0cd8fddddc80cbab988
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-02-07 11:38:02 -0300

    Code cleanup.

--- src/aslib/util/NumericType.java
+++ src/aslib/util/NumericType.java
@@ -1,37 +1,41 @@
 package aslib.util;
 
 /**
- * <p> Contains the options for all numeric class in Java. </p>
+ * <p> Contains the options to represent all the numeric class in Java. </p>
  *
  * <table style="width:150px" summary="">
- * <tr>
- * <th> Type </th>
- * <th> Class </th>
- * </tr>
- * <tr>
- * <td> Byte </td>
- * <td> Byte.class </td>
- * </tr>
- * <tr>
- * <td> Short </td>
- * <td> Short.class </td>
- * </tr><tr>
- * <td> Integer </td>
- * <td> Integer.class </td>
- * </tr><tr>
- * <td> Long </td>
- * <td> Long.class </td>
- * </tr><tr>
- * <td> Float </td>
- * <td> Float.class </td>
- * </tr><tr>
- * <td> Double </td>
- * <td> Double.class </td>
- * </tr>
+ *  <tr>
+ *      <th> Type </th>
+ *      <th> Class </th>
+ *  </tr>
+ *  <tr>
+ *      <td> Byte </td>
+ *      <td> Byte.class </td>
+ *  </tr>
+ *  <tr>
+ *      <td> Short </td>
+ *      <td> Short.class </td>
+ *  </tr>
+ *  <tr>
+ *      <td> Integer </td>
+ *      <td> Integer.class </td>
+ *  </tr>
+ *  <tr>
+ *      <td> Long </td>
+ *      <td> Long.class </td>
+ *  </tr>
+ *  <tr>
+ *      <td> Float </td>
+ *      <td> Float.class </td>
+ *  </tr>
+ *  <tr>
+ *      <td> Double </td>
+ *      <td> Double.class </td>
+ *  </tr>
  * </table>
  *
  * @author Adriano Siqueira
- * @version 2019-05-03
+ * @version 3.0.0
  * @since 6.1
  */
 public enum NumericType {
@@ -42,9 +46,18 @@ public enum NumericType {
     FLOAT(Float.class),
     DOUBLE(Double.class);
 
-    public final Class numericClass;
+    private final Class<?> numericClass;
 
-    NumericType(Class numericClass) {
+    NumericType(Class<?> numericClass) {
         this.numericClass = numericClass;
+    }
+
+    /**
+     * Gets the numeric type's class.
+     *
+     * @return The class of the numeric type.
+     */
+    public Class<?> getNumericClass() {
+        return numericClass;
     }
 }
