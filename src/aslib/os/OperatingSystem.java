b79c575440299b7f1829218b81a632e23e545fb5
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-10-26 11:07:53 -0300

    New class to represent operating systems.

---
+++ src/aslib/os/OperatingSystem.java
@@ -0,0 +1,26 @@
+package aslib.os;
+
+/**
+ * <p> Contains the options that represent an Operating System. </p>
+ *
+ * @author Adriano Siqueira
+ * @version 4.0.0
+ * @since 6.1.0
+ */
+public enum OperatingSystem {
+
+    /**
+     * @since 1.0.0
+     */
+    LINUX,
+
+    /**
+     * @since 1.0.0
+     */
+    MACOS,
+
+    /**
+     * @since 1.0.0
+     */
+    WINDOWS
+}
