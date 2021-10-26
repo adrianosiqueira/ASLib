7f79314596e0d817bd26fcfa9f6de8c1b649b57d
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-10-26 11:14:11 -0300

    Improvements in javadoc.

--- src/aslib/util/NumericType.java
+++ src/aslib/util/NumericType.java
@@ -5,38 +5,38 @@ package aslib.util;
  *
  * <table style="width:150px" summary="">
  *  <tr>
- *      <th> Type </th>
- *      <th> Class </th>
+ *      <th>Type</th>
+ *      <th>Class</th>
  *  </tr>
  *  <tr>
- *      <td> Byte </td>
- *      <td> Byte.class </td>
+ *      <td>Byte</td>
+ *      <td>{@link Byte}.class</td>
  *  </tr>
  *  <tr>
- *      <td> Short </td>
- *      <td> Short.class </td>
+ *      <td>Short</td>
+ *      <td>{@link Short}.class</td>
  *  </tr>
  *  <tr>
- *      <td> Integer </td>
- *      <td> Integer.class </td>
+ *      <td>Integer</td>
+ *      <td>{@link Integer}.class</td>
  *  </tr>
  *  <tr>
- *      <td> Long </td>
- *      <td> Long.class </td>
+ *      <td>Long</td>
+ *      <td>{@link Long}.class</td>
  *  </tr>
  *  <tr>
- *      <td> Float </td>
- *      <td> Float.class </td>
+ *      <td>Float</td>
+ *      <td>{@link Float}.class</td>
  *  </tr>
  *  <tr>
- *      <td> Double </td>
- *      <td> Double.class </td>
+ *      <td>Double</td>
+ *      <td>{@link Double}.class</td>
  *  </tr>
  * </table>
  *
  * @author Adriano Siqueira
  * @version 3.0.0
- * @since 6.1
+ * @since 6.1.0
  */
 public enum NumericType {
     BYTE(Byte.class),
