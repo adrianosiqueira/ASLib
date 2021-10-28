f34236351ae3dff2fe2f7ce80ad58cef28e95817
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2021-10-28 16:59:53 -0300

    Updated to use new parse API.

--- src/aslib/security/HashCalculator.java
+++ src/aslib/security/HashCalculator.java
@@ -1,6 +1,7 @@
 package aslib.security;
 
-import aslib.util.DataConverter;
+import aslib.parse.BytesToStringParser;
+import aslib.parse.PrimitiveWrapperArrayParser;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -9,71 +10,59 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Objects;
 
 /**
- * <p> Provides several ways to calculate the hash checksum. </p>
+ * <p>Dedicated class for calculating hash checksums.</p>
  *
  * @author Adriano Siqueira
- * @version 2.0.1
+ * @version 2.0.2
  * @since 9.0.0
  */
 public class HashCalculator {
 
     /**
-     * <p> Creates a {@link HashCalculator} instance. </p>
+     * <p>Creates an instance of the {@link HashCalculator} class.</p>
      *
      * @since 2.0.0
      */
     public HashCalculator() {
     }
 
+
     /**
-     * <p> Calculates the hash checksum from an array of bytes. </p>
+     * <p>Calculates the hash checksum from a {@link Path}.</p>
      *
-     * @param shaType Defines which hash algorithm will be used.
-     * @param bytes   Array which will be used to calculate the hash.
+     * @param shaType Defines which hash algorithm will be used. Not null.
+     * @param path    Path to a regular file which will be used to calculate the
+     *                hash.
      *
      * @return The calculated hash in a String format.
      *
-     * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, byte[] bytes)
-    throws NullPointerException {
-        Objects.requireNonNull(shaType, "ShaType can not be null.");
-
-        try {
-            MessageDigest digest = MessageDigest.getInstance(shaType.getName());
-            byte[]        result = digest.digest(bytes);
-            return DataConverter.byteToString(result);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return "";
-        }
+    public final String calculate(SHAType shaType, Path path) {
+        return calculate(shaType, path.toFile());
     }
 
     /**
-     * <p> Calculates the hash checksum from a {@link File}. </p>
+     * <p>Calculates the hash checksum from a {@link File}.</p>
      *
-     * @param shaType Defines which hash algorithm will be used.
-     * @param file    File which will be used to calculate the hash.
+     * @param shaType Defines which hash algorithm will be used. Not null.
+     * @param file    Regular file which will be used to calculate the hash.
      *
      * @return The calculated hash in a String format.
      *
-     * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, File file)
-    throws NullPointerException {
-        Objects.requireNonNull(shaType, "ShaType can not be null.");
-
+    public final String calculate(SHAType shaType, File file) {
         int    bufferSize  = 2048;
         byte[] bytesBuffer = new byte[bufferSize];
 
         try (
-                BufferedInputStream stream = new BufferedInputStream(new FileInputStream(file),
-                                                                     bufferSize)
+                BufferedInputStream stream = new BufferedInputStream(
+                        new FileInputStream(file),
+                        bufferSize
+                )
         ) {
             MessageDigest digest = MessageDigest.getInstance(shaType.getName());
             int           read;
@@ -82,8 +71,10 @@ public class HashCalculator {
                 digest.update(bytesBuffer, 0, read);
             }
 
-            byte[] result = digest.digest();
-            return DataConverter.byteToString(result);
+            Byte[] result = PrimitiveWrapperArrayParser
+                    .toWrapper(digest.digest());
+
+            return new BytesToStringParser().parse(result);
         } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
             return "";
@@ -91,34 +82,39 @@ public class HashCalculator {
     }
 
     /**
-     * <p> Calculates the hash checksum from a {@link Path}. </p>
+     * <p>Calculates the hash checksum from a {@link String}.</p>
      *
-     * @param shaType Defines which hash algorithm will be used.
-     * @param path    Path which will be used to calculate the hash.
-     *
-     * @return The calculated hash in a String format.
-     *
-     * @throws NullPointerException If shaType is null.
-     * @since 2.0.0
-     */
-    public final String calculate(SHAType shaType, Path path)
-    throws NullPointerException {
-        return calculate(shaType, path.toFile());
-    }
-
-    /**
-     * <p> Calculates the hash checksum from a {@link String}. </p>
-     *
-     * @param shaType Defines which hash algorithm will be used.
+     * @param shaType Defines which hash algorithm will be used. Not null.
      * @param string  String which will be used to calculate the hash.
      *
      * @return The calculated hash in a String format.
      *
-     * @throws NullPointerException If shaType is null.
      * @since 2.0.0
      */
-    public final String calculate(SHAType shaType, String string)
-    throws NullPointerException {
+    public final String calculate(SHAType shaType, String string) {
         return calculate(shaType, string.getBytes());
+    }
+
+    /**
+     * <p>Calculates the hash checksum from an array of bytes.</p>
+     *
+     * @param shaType Defines which hash algorithm will be used. Not null.
+     * @param bytes   Array which will be used to calculate the hash.
+     *
+     * @return The calculated hash in a String format.
+     *
+     * @since 2.0.0
+     */
+    public final String calculate(SHAType shaType, byte[] bytes) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(shaType.getName());
+
+            Byte[] result = PrimitiveWrapperArrayParser
+                    .toWrapper(digest.digest(bytes));
+
+            return new BytesToStringParser().parse(result);
+        } catch (NoSuchAlgorithmException ignored) {
+            return "";
+        }
     }
 }
