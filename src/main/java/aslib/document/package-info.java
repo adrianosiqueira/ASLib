aa7a8328599edd4fee2e8735bd3a6e3df650a3da
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-07-04 18:58:03 -0300

    Add 'package-info'.

---
+++ src/main/java/aslib/document/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ * This API is subdivided into dedicated packages for each of the supported
+ * countries, using the <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#Officially_assigned_code_elements">ISO 3166-1 alpha-3</a>
+ * standardization code to name the packages.
+ * </p>
+ *
+ * @author Adriano Siqueira
+ * @version 1.0.0
+ * @since 12.0.0
+ */
+package aslib.document;
