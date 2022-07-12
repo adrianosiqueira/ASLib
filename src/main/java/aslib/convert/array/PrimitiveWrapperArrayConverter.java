902d65c1290189fe8620cea86cab18b9dcd05094
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-07-12 17:27:13 -0300

    Add 'PrimitiveWrapperArrayConverter' class.

---
+++ src/main/java/aslib/convert/array/PrimitiveWrapperArrayConverter.java
@@ -0,0 +1,280 @@
+package aslib.convert.array;
+
+/**
+ * <p style="text-align:justify">
+ * Handles arrays conversions between primitive and wrapper types.
+ * </p>
+ *
+ * @author Adriano Siqueira
+ * @version 1.0.0
+ * @since 12.0.0
+ */
+public class PrimitiveWrapperArrayConverter {
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static byte[] toPrimitive(Byte[] array) {
+        byte[] result = new byte[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static char[] toPrimitive(Character[] array) {
+        char[] result = new char[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static double[] toPrimitive(Double[] array) {
+        double[] result = new double[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static float[] toPrimitive(Float[] array) {
+        float[] result = new float[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static int[] toPrimitive(Integer[] array) {
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static long[] toPrimitive(Long[] array) {
+        long[] result = new long[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the wrapper array to a primitive array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of primitive type.
+     *
+     * @since 1.0.0
+     */
+    public static short[] toPrimitive(Short[] array) {
+        short[] result = new short[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Byte[] toWrapper(byte[] array) {
+        Byte[] result = new Byte[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Character[] toWrapper(char[] array) {
+        Character[] result = new Character[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Double[] toWrapper(double[] array) {
+        Double[] result = new Double[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Float[] toWrapper(float[] array) {
+        Float[] result = new Float[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Integer[] toWrapper(int[] array) {
+        Integer[] result = new Integer[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Long[] toWrapper(long[] array) {
+        Long[] result = new Long[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+
+    /**
+     * <p style="text-align:justify">
+     * Converts the primitive array to a wrapper array.
+     * </p>
+     *
+     * @param array Array that will be converted.
+     *
+     * @return The array of wrapper type.
+     *
+     * @since 1.0.0
+     */
+    public static Short[] toWrapper(short[] array) {
+        Short[] result = new Short[array.length];
+
+        for (int i = 0; i < array.length; i++) result[i] = array[i];
+
+        return result;
+    }
+}
