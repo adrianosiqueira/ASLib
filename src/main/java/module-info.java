2cc031c1967baacbc603c8459add8bd5fb843f0f
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-07-22 18:18:20 -0300

    Remove unused packages.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,30 +2,20 @@ module ASLib {
     requires transitive javafx.controls;
 
     exports aslib.ai.genderdetector;
-    exports aslib.cli;
     exports aslib.document;
     exports aslib.document.bra;
-    exports aslib.exceptions;
-    exports aslib.filemanager;
+    exports aslib.javafx.filemanager;
     exports aslib.javafx.dialog;
     exports aslib.network;
     exports aslib.operatingsystem;
-    exports aslib.parse;
-    exports aslib.security;
-    exports aslib.time;
     exports aslib.util;
 
     opens aslib.ai.genderdetector;
-    opens aslib.cli;
     opens aslib.document;
     opens aslib.document.bra;
-    opens aslib.exceptions;
-    opens aslib.filemanager;
+    opens aslib.javafx.filemanager;
     opens aslib.javafx.dialog;
     opens aslib.network;
     opens aslib.operatingsystem;
-    opens aslib.parse;
-    opens aslib.security;
-    opens aslib.time;
     opens aslib.util;
 }
